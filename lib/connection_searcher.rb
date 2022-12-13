239bfb2be9c593cfc8cc477ec8a7fa335a9437a2
Matt Burns <msb@ameritech.net>
2022-12-13 08:10:54 -0500

    fix connection searcher

--- lib/connection_searcher.rb
+++ lib/connection_searcher.rb
@@ -6,7 +6,23 @@ class ConnectionSearcher
     @slots = slots
   end
 
-  def search(slot, stack = [], depth = 0)
-    puts "Hey, at least I tried!"
+  def search(slot, color = nil, direction = nil, stack = [], visited = [], depth = 0)
+    # Add neighbors
+    slot.neighbors.each { |slot_i| stack << slot_i }
+    visited << slot 
+    
+    # If slot is colored, check neghbors for same color
+    if slot.color
+      next_slot = stack.pop
+      next_direction = slot.neig_directions[next_slot]
+      search(
+        next_slot,
+        slot.color,
+        next_direction,
+        stack,
+        visited,
+        (depth + 1)
+      )
+    end
   end
 end
